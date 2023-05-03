2b77998fd88c7803a9fd3f653eae8c6d835e0824
Brandon Mountan <brandonmountan1@gmail.com>
2023-05-03 17:54:15 -0600

    getting started

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var num = prompt('Please enter a numeric value between 8 and 128');
+  if (num < 9 || num > 128) {
+    alert ('not a valid length');
+  }
+  var numbers = confirm("Do you want numbers in your password? (yes/no)");
+
+}
+
+
+
+
+  // password.length > 8 || password.length < 128
