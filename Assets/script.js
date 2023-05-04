2f421cdccda1089c43b9757d2d51f30bc31abcdc
Brandon Mountan <brandonmountan1@gmail.com>
2023-05-04 14:34:55 -0600

    prompting

--- Assets/script.js
+++ Assets/script.js
@@ -8,20 +8,37 @@ function writePassword() {
   passwordText.value = password;
 }
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  var num = prompt('Please enter a numeric value between 8 and 128');
-  if (num < 9 || num > 128) {
-    alert ('not a valid length');
-  }
-  var numbers = confirm("Do you want numbers in your password? (yes/no)");
+
+var lengthAnswer = prompt('Please enter a numeric value between 8 and 128 to determine the length of your password');
+
+if (lengthAnswer < 9 || lengthAnswer > 128) {
+
+  alert ('not a valid length');
+
+  prompt('Please enter a numeric value between 8 and 128 to determine the length of your password');
+
+};
+
+console.log(lengthAnswer);
+
+var numbersAnswer = confirm("Do you want numbers in your password? (OK(yes)/Cancel(no))");
+
+console.log(numbersAnswer);
+
+var upperCaseAnswer = confirm("Do you want upper case letters in your password? (OK(yes)/Cancel(no))");
+
+console.log(upperCaseAnswer);
+
+var lowerCaseAnswer = confirm("Do you want lower case letters in your password? (OK(yes)/Cancel(no))");
+
+console.log(lowerCaseAnswer);
+
+var symbolsAnswer = confirm("Do you want symbols in your password? (OK(yes)/Cancel(no))");
+
+console.log(symbolsAnswer);
 
 }
-
-
-
-
-  // password.length > 8 || password.length < 128
